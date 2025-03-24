6f77b6057394b1627ee49468fc7b30d4041917ca
hardik01work <hardik01.work@gmail.com>
2025-03-24 20:10:06 +0530

    Updated CSS Files
    Tailwind, Native wind, CSS

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -11,10 +11,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Link href="../signIn">Sign In</Link>
-      <Link href="../explore">Explore</Link>
-      <Link href="../profile">Profile</Link>
-      <Link href="../properties/1">Property</Link>
+      <Link href="/root/tabs/signIn">Sign In</Link>
+      <Link href="/root/tabs/explore">Explore</Link>
+      <Link href="/root/tabs/profile">Profile</Link>
+      <Link href="/root/properties/1">Property</Link>
       
     </View>
   );
